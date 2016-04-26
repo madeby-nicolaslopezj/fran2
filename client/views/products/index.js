698c9c8af2c392066695934ea6293a4d66fbbe94
Nicolás López <nicolaslopezj@me.com>
2016-04-25 22:15:48 -0300

    fix seo

--- client/views/products/index.js
+++ client/views/products/index.js
@@ -3,6 +3,25 @@ Template.productsIndex.onCreated(function() {
   this.selectedProducts = new ReactiveVar([]);
 });
 
+Template.productsIndex.onRendered(function() {
+  this.autorun(function() {
+    SEO.set({
+      title: 'Venta - ' + orion.dictionary.get('seo.title'),
+      link: {
+        icon: Meteor.absoluteUrl().slice(0, -1) + orion.dictionary.get('seo.favicon.url'),
+      },
+      meta: {
+        description: orion.dictionary.get('seo.description'),
+      },
+      og: {
+        title: 'Venta - ' + orion.dictionary.get('seo.title'),
+        description: orion.dictionary.get('seo.description'),
+        image: Meteor.absoluteUrl().slice(0, -1) + orion.dictionary.get('seo.image.url'),
+      },
+    });
+  });
+});
+
 Template.productsIndex.helpers({
   products: function() {
     return Products.find({}, { sort: { index: 1 } }).fetch();
@@ -19,6 +38,9 @@ Template.productsIndex.helpers({
 Template.productsIndex.events({
   'click .store-col': function(event, template) {
     if ($(event.target).hasClass('thumb-box')) {
+      return;
+    }
+    if (Session.get('contactProductsSent')) {
       return;
     }
     const selected = template.selectedProducts.get();
