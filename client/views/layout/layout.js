aa050b5fd775daac4578b65318f2019b94a5d401
Nicolás López <nicolaslopezj@me.com>
2016-04-27 14:36:10 -0300

    google analytics

--- client/views/layout/layout.js
+++ client/views/layout/layout.js
@@ -1,5 +1,12 @@
 Template.layout.onCreated(function() {
   this.subscribe('categories');
+
+  var gaScript = 'https://www.google-analytics.com/analytics.js';
+  DocHead.loadScript(gaScript, function() {
+    console.log('Google Analytics loaded');
+    ga('create', Meteor.settings.public.ga.id, 'auto');
+    ga('send', 'pageview');
+  });
 });
 
 Template.layout.helpers({
