81d6e95e2b28e60ff2b97e3b8619c3799a2270f0
Nicolás López <nicolaslopezj@me.com>
2018-08-29 17:34:37 -0300

    remove link to open image

--- client/views/products/item.js
+++ client/views/products/item.js
@@ -46,10 +46,10 @@ Template.productsIndexItem.events({
   'click .thumb-box': function(event, template) {
     template.selectedImage.set(this);
   },
-  'click .show-image-box': function(event, template) {
-    const image = template.selectedImage.get();
-    window.open(image.url);
-  },
+  // 'click .show-image-box': function(event, template) {
+  //   const image = template.selectedImage.get();
+  //   window.open(image.url);
+  // },
   'click .btn-select': function(event, template) {
     const selected = Session.get('selectedProducts');
     const item = _.findWhere(selected, { _id: this._id });
